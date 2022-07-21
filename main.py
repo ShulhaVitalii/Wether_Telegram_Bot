0026c1288a88255424ce2f08bae6683998c65a0e
VitaliiAQA <94836456+VitaliiAQA@users.noreply.github.com>
2022-07-21 14:08:15 +0300

    modifying bot to send weather photo update requirements.txt

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import telebot
 from telebot import types
-from weather import get_city_weather
+from weather import get_city_weather, get_city_weather_screen
 
 bot = telebot.TeleBot('5570945317:AAGuWxuIMrPSTcZi22uk1eeoMV7rauA09zg')
 
@@ -12,7 +12,7 @@ def start(message):
     bot.send_message(message.chat.id, mess, parse_mode='html')
 
     keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
-    buttons = ['Kyiv', 'Lviv', 'Odesa', 'Charkov', 'Pirnove']
+    buttons = ['Kyiv', 'Lviv', 'Odesa', 'Kharkiv', 'Pirnove']
     keyboard.add(*buttons)
     bot.send_message(message.chat.id, f"Or chose the city in the bot's menu.", reply_markup=keyboard)
 
@@ -20,8 +20,11 @@ def start(message):
 @bot.message_handler(func=lambda message: True)
 def get_weather_from_text(message):
     city = message.text
-    weather = get_city_weather(city)
-    bot.send_message(message.chat.id, weather,  parse_mode='html')
+    get_city_weather_screen(city)
+    pict = open('screenshot.png', 'rb')
+    bot.send_photo(message.chat.id, pict)
+    w = get_city_weather(city)
+    bot.send_message(message.chat.id, w)
 
 
 bot.infinity_polling()
