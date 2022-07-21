555501725db0528419cb2b1ae859496edb3b714d
VitaliiAQA <94836456+VitaliiAQA@users.noreply.github.com>
2022-07-21 14:06:51 +0300

    added method to get weather screenshot

--- weather.py
+++ weather.py
@@ -1,4 +1,15 @@
+from PIL import Image
+from io import BytesIO
+# python3 -m pip install --upgrade pip
+# python3 -m pip install --upgrade Pillow
+
 import requests
+
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+
+from webdriver_manager.chrome import ChromeDriverManager
 
 
 def get_city_weather(city):
@@ -13,5 +24,40 @@ def get_city_weather(city):
 
     return f'In {city.title()} now {weather["weather"]}, the temperature is {weather["temp"]} Celsius, ' \
            f'feels like {weather["feels_like"]}, and winds {weather["wind"]} meters per second.'
+
+
+def get_city_weather_screen(city):
+    api_key = '745fb1f9f05d9447185ec6e2868e1ca5'
+    url = f'https://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}&units=metric&mode=html'
+    get_screenshot_html(url)
+
+
+def get_html(response):
+    with open("current_weather.html", 'w') as file:
+        file.writelines(response.text)
+
+
+def get_screenshot_html(url):
+    chrome_options = Options()
+    chrome_options.add_argument("--headless")
+    driver = webdriver.Chrome(ChromeDriverManager().install(), options=chrome_options)
+    driver.get(url)
+
+    element = driver.find_element(By.TAG_NAME, 'body')
+    location = element.location
+    png = driver.get_screenshot_as_png()
+    driver.quit()
+
+    im = Image.open(BytesIO(png))
+    left = 0
+    top = 0
+    right = location['x'] + 130
+    bottom = location['y'] + 135
+
+    im = im.crop((left, top, right, bottom))  # defines crop points
+    im.save('screenshot.png')  # saves new cropped image
+
+
+get_city_weather_screen('kyiv')
 
 
